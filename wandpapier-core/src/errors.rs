fcdec0c1e396effb5cdb6226d496137fc37bbc5b
Paul K <pa179kin@gmail.com>
2025-09-19 13:43:36 +0200

    add thiserror + Errors enum

---
+++ wandpapier-core/src/errors.rs
@@ -0,0 +1,9 @@
+use std::io;
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum Errors {
+    #[error("error while creating directory.")]
+    DirCreate(#[from] io::Error),
+    #[error("error in libheif-rs.")]
+    Hife(#[from] libheif_rs::HeifError)
+}
